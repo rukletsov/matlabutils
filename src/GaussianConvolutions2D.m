d60130578adadfce79b06fcf0bc7816d11ca5b81
Alexander Rukletsov <alexander.rukletsov@ziti.uni-heidelberg.de>
2011-10-25 18:12:07 +0200

    Convolutions with gaussian 2D kernels added.

---
+++ src/GaussianConvolutions2D.m
@@ -0,0 +1,24 @@
+
+% Convolves given image using one of the gaussian kernels. See notes on
+% GaussianKernels2D() function for more information about available
+% kernels.
+%
+% Inputs.
+%   I:      Input image (discrete brightness function).
+%   sigma:  Gaussian sigma.
+%   type:   Indicates which derivative to take. Can be one from {'0', 'x',
+%           'y', 'xx', 'xy', 'yy'}. Value '0' means standard gaussian, no
+%           derivative is taken.
+% Outputs.
+%   J:      Convolved image of type double.
+%
+% Copyright (c) 2011 Alexander Rukletsov <rukletsov@gmail.com>
+% All rights reserved.
+
+function J = GaussianConvolutions2D(I, sigma, type)
+
+  % Get appropriate kernel.
+  kernel = GaussianKernels2D(sigma, type);
+  J = imfilter(double(I), kernel, 'conv', 'symmetric');
+
+% end GaussianConvolutions2D()
