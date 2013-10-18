c40e71fc09e7200dfea944126f67469963b744bb
Alexander Rukletsov <alexander.rukletsov@cathi.eu>
2013-10-18 23:54:25 +0200

    Simplify SaveToRaw().

--- src/SaveToRaw.m
+++ src/SaveToRaw.m
@@ -1,40 +1,26 @@
 
-% Saves the provided array into as a raw image.
+% Saves the provided array as a raw image.
 %
 % Inputs.
-%   image:  2D or 3D array containing image data.
-%   path:   Path to the output file.
-%   transp: Whether transpose image before saving. Can be either true or
-%           false.
-%   bpp:    Specifies the colour depth of the raw image. Can be one from
-%           {8, 16, 32, 64}.
+%   image:        2D or 3D array containing image intensities.
+%   path:         Path to the output file.
+%   transp:       Whether transpose image before saving. Can be either true or
+%                 false.
+%   format_spec:  Format of the input image. Can be one from
+%                 {'uint8', 'uint16', 'uint32', 'uint64'}.
 %
 % Example.
 %   SaveToRaw(img, '128x128_8bpp.raw', true, 8) saves the provided matrix
 %   into a 8-bit raw image, applying transpose operation first.
 %
-% Copyright (c) 2012 Alexander Rukletsov <rukletsov@gmail.com>
+% Copyright (c) 2012, 2013 Alexander Rukletsov <rukletsov@gmail.com>
 % All rights reserved.
 
-function SaveToRaw(image, path, transp, bpp)
+function SaveToRaw(image, path, transp, format_spec)
 
   if (transp)
     image = image.';
-  end
-  
-  format_spec = '';
-  switch bpp
-  case 8
-    format_spec = 'uint8';
-  case 16
-    format_spec = 'uint16';
-  case 32
-    format_spec = 'uint32';
-  case 64
-    format_spec = 'uint64';
-  otherwise
-    cout('Specified bpp is not supported.');
-  end  
+  end 
 
   fid = fopen(path, 'w+');
   fwrite(fid, image, format_spec);
